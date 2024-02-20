214a49acb39f8b49059c28824a8f2d13b543ce20
Dalibor Labudovic <sherkon18@gmail.com>
2024-02-20 13:17:27 -0500

    Create check_systemarg.py
    initial commit

---
+++ check_systemarg.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import argparse
+
+#Function to check if the website is up
+def is_website_up(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        return False
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="The website you want to check.")
+    args = parser.parse_args()
+    url = args.url
+    
+    if not url.startswith("http"):
+        url = "https://" + url
+    if is_website_up(url):
+        print(f"{url} is up!")
+    else:
+        print(f"{url} is down!")
+
+if __name__ == "__main__":
+    main()
